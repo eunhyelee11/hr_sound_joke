0b60336dab7c1ade47167592d92aa01d05a4108d
eunhyelee11 <35700469+eunhyelee11@users.noreply.github.com>
2023-05-05 10:27:31 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.py
@@ -0,0 +1,5 @@
+basic.show_number(7)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("A A G A A A G G ", 380)
+    music.playMelody("G G - - - - - - ", 600)
+    music.playMelody("E E D E E E D D ", 380)
+    music.playMelody("D D - - - - - - ", 600)
+})
+basic.forever(function () {
+	
+})
